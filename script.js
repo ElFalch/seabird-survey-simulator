7e5648efc43398e0cc3417a10ebf97fb1282744c
Eleanor Falch <eleanorfalch95@gmail.com>
2025-01-23 14:35:17 +0000

    feat: display image div when start button clicked*
    *image of bird in left column

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const startButton = document.getElementById("start")
+let portBird = document.getElementById("port-bird")
 
 startButton.addEventListener("click", startSurvey)
+
+// hide start button and show first bird image when start button is clicked
 
 function startSurvey() {
     console.log("Seabird survey started")
     startButton.classList.add("hidden")
+    portBird.classList.remove("hidden")
 }
+
