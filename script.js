57d7d5d10696681f8186e119b4b98ffb8759d763
Eleanor Falch <eleanorfalch95@gmail.com>
2025-01-27 15:27:43 +0000

    feat: store src and answer for each image*
    *and log each value

--- script.js
+++ script.js
@@ -6,23 +6,24 @@ let starBird = document.getElementById("star-bird")
 
 startButton.addEventListener("click", startSurvey)
 
+// correct answer and src for each image
+
+let questions = {
+    question: ["https://8000-elfalch-seabirdsurveysi-gpj5xixjgi7.ws.codeinstitute-ide.net/assets/images/puffin.webp","https://8000-elfalch-seabirdsurveysi-gpj5xixjgi7.ws.codeinstitute-ide.net/assets/images/guillemot.webp"],
+    answer: ["3", "2"]
+};
+
 // hide start button, show first bird image and save its src when start button is clicked
 
 function startSurvey() {
     console.log("Seabird survey started");
     startButton.classList.add("hidden");
     portBird.classList.remove("hidden");
-    let src = portBird.src ;
-    console.log(src);
+    console.log(questions.question[0]);
+    console.log(questions.answer[0]);
+    console.log(questions.question[1]);
+    console.log(questions.answer[1]);
 }
-
-// correct answer and src for each image
-
-const questions = {
-        question: "https://8000-elfalch-seabirdsurveysi-gpj5xixjgi7.ws.codeinstitute-ide.net/assets/images/puffin.webp",
-        answer: "3"
-    };
-
 
 // store submitted value, assess and log if it is correct or not 
 
