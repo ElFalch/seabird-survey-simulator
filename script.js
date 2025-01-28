ad3ae956b52015badd8a9304de162846e1cc765c
Eleanor Falch <eleanorfalch95@gmail.com>
2025-01-28 14:24:50 +0000

    fix: make second bird image random

--- script.js
+++ script.js
@@ -58,13 +58,12 @@ function nextSurvey() {
     nextButton.classList.add("hidden");
     correct.classList.add("hidden");
     portBird.classList.add("hidden");
+    randomBirdIndex = Math.floor(Math.random() * questions.question.length);
     starBird.src = questions.question[randomBirdIndex];
     starBird.alt = questions.alt[randomBirdIndex];
     starBird.classList.remove("hidden");
     submit.classList.remove("hidden");
     form.sppCode.value = "0";
-    let src = starBird.src ;
-    console.log(src);
 }
 
 // reset form when try again button is clicked 
