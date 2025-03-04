fd5a56e87a88b813c20d0be9855e8135ebb5185f
ElFalch <eleanorfalch95@gmail.com>
2025-03-04 14:16:10 +0000

    fix: replace & signs with commas*
    * to join multiple if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,8 +168,8 @@ document.addEventListener("DOMContentLoaded", function () {
     if (questions === sitting) {
       let sidePS = formData.side.value;
       if (
-        (sppCode === questions.sppAnswer[randomBirdIndex]) &
-        (number === questions.numAnswer[randomBirdIndex]) &
+        (sppCode === questions.sppAnswer[randomBirdIndex]), 
+        (number === questions.numAnswer[randomBirdIndex]), 
         (sidePS === side[randomSide])
       ) {
         right.classList.remove("hidden");
